8f473a43118225c3213c0cb40c00e8be3eae5502
Никита Виноградов <N0975426435@gmail.com>
2021-07-13 17:01:31 +0300

    Add models!

--- app_format/main/admin.py
+++ app_format/main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Trainer
+from .models import Client
+from .models import Training
+from .models import Subscription
 
-# Register your models here.
+admin.site.register(Trainer)
+admin.site.register(Client)
+admin.site.register(Training)
+admin.site.register(Subscription)
+
--- app_format/main/models.py
+++ app_format/main/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Trainer(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=30)
+
+
+class Client(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=30)
+    date_birth = models.DateField()
+    active = models.BooleanField()
+
+
+class Subscription(models.Model):
+    id = models.IntegerField(primary_key=True)
+    training_count = models.IntegerField(default=10)
+    price = models.IntegerField()
+    date_start = models.DateField()
+    month_count = models.IntegerField(default=1)
+    client_id = models.ForeignKey('Client', on_delete=models.SET_NULL, null=True)
+    type = models.CharField(max_length=5)
+
+
+class Training(models.Model):
+    id = models.IntegerField(primary_key=True)
+    date_time = models.DateTimeField()
+    trainer_id = models.ForeignKey('Trainer', on_delete=models.SET_NULL, null=True)  # set restrict or set_null?????
+    clients = models.ManyToManyField(Client)
+
